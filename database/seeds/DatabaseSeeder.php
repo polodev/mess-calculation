4f783fef90d4fc24e8e1ccba0e5d790d3cebb6a1
polodev <polodev10@gmail.com>
2019-05-26 17:45:34 +0600

    ______

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call(MealsTableSeeder::class);
         $this->call(BazarsTableSeeder::class);
         $this->call(DebitcreditsTableSeeder::class);
+        $this->call(UserMonthsTableSeeder::class);
       }
     }
   }
